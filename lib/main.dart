3d58f6a7c19cc68bed432882289b20177ef2a9de
Nita <loutnita68@gmail.com>
2026-06-28 14:11:24 +0700

    tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Homepage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Pages());
   }
 }
